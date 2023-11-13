76de852bcc9a973fa048210a02853bbc13de3012
CaianSantana <caian23@outlook.com>
2023-11-13 12:49:41 -0300

    Fruit

--- Main.py
+++ Main.py
@@ -1,10 +1,14 @@
 import pygame, sys
+from Fruit import Fruit
 
 pygame.init()
 cellSize = 40
 cellNumber = 20
 screen = pygame.display.set_mode((cellSize*cellNumber, cellSize*cellNumber))
 clock = pygame.time.Clock()
+
+fruit = Fruit(cellNumber, cellSize, screen)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -13,3 +17,4 @@ while True:
     pygame.display.update()
     clock.tick(60)
     screen.fill((175,215,70))
+    fruit.drawFruit()
