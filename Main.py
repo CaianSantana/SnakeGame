62a2c139d738f5879fb721a57375e701bdb205c2
CaianSantana <caian23@outlook.com>
2023-11-17 10:10:22 -0300

    Font score

--- Main.py
+++ Main.py
@@ -10,6 +10,8 @@ class Main:
         self.cellNumber = cellNumber
         self.cellSize = cellSize
         self.screen = screen
+        self.score = 0
+        self.gameFont = pygame.font.Font('Fonts/Retro-Gaming.ttf', 25)
         
     def update(self):
         self.snake.move()
@@ -19,12 +21,12 @@ class Main:
     def draw(self):
         self.drawGrass()
         self.drawElements()
+        self.drawScore()
         
     
     def drawElements(self):
         self.fruit.draw(self.cellSize, self.screen)
         self.snake.draw(self.cellSize, self.screen)
-        
     def drawGrass(self):
         grassColor = (167,209,61)
         for row in range(self.cellNumber):
@@ -38,6 +40,17 @@ class Main:
                     if col %2 != 0:
                         grassRect = pygame.Rect(col*self.cellSize, row* self.cellSize, self.cellSize, self.cellSize)
                         pygame.draw.rect(self.screen, grassColor, grassRect)
+    def drawText(self):
+        text = self.score
+        size = size(text)
+        line = line(text)
+    def drawScore(self):
+        scoreText = str(len(self.snake.body)-3)
+        scoreSurface = self.gameFont.render(scoreText, False, (0, 0, 0))
+        scoreX = int(self.cellSize*self.cellNumber - 60)
+        scoreY = int(self.cellSize * self.cellNumber - 40)
+        scoreRect = scoreSurface.get_rect(center = (scoreX, scoreY))
+        self.screen.blit(scoreSurface, scoreRect)
     
     def keyInput(self, event):
         self.snake.changeDirection(event)
@@ -53,6 +66,12 @@ class Main:
         for block in self.snake.body[1:]:
             if block == self.snake.body[0]:
                 self.gameOver()
+                
+    def line(self, text, color=(255,255,255), antialias=False, background=None):
+        return pygame.font.Font.render(text, antialias, color, background)
+    def size(self, text):
+        return pygame.font.Font.size(self.cellSize, text)
+        
             
     def gameOver(self):
         print("Game Over")
